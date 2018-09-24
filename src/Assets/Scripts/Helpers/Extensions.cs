b530443773468016f739f1514ea343e1e1811d56
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-09-24 02:10:32 -0300

    Adicionado classe para Serializa e Deserializa Json

---
+++ src/Assets/Scripts/Helpers/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Motherload.Core.Helpers
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Retorna todos os pisos <see cref="T"/>
+        /// </summary>
+        /// <typeparam name="T">Piso buscado</typeparam>
+        /// <param name="tilemap">Tilemap a ser buscado</param>
+        /// <returns></returns>
+        public static T[] GetTiles<T>(this Tilemap tilemap) where T : TileBase
+        {
+            List<T> tiles = new List<T>();
+
+            for (int y = tilemap.origin.y; y < (tilemap.origin.y + tilemap.size.y); y++)
+            {
+                for (int x = tilemap.origin.x; x < (tilemap.origin.x + tilemap.size.x); x++)
+                {
+                    T tile = tilemap.GetTile<T>(new Vector3Int(x, y, 0));
+                    if (tile != null)
+                    {
+                        tiles.Add(tile);
+                    }
+                }
+            }
+            return tiles.ToArray();
+        }
+    }
+}
---
+++ src/Assets/Scripts/Helpers/GameJson.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace Motherload.Core.Helpers
+{
+    /// <summary>
+    /// Classe que lida com Serialização e Deserialização em Json.
+    /// </summary>
+    public class GameJson
+    {
+        /// <summary>
+        /// Serializa um arquivo para Json
+        /// </summary>
+        /// <param name="path">Arquivo para qual serializiar</param>
+        /// <param name="value">Objeto a ser serializado</param>
+        /// <returns></returns>
+        public static bool Serialize(string file, object value)
+        {
+            try
+            {
+                var jsonFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), file);
+
+                File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(value, Formatting.None));
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Deserializa um arquivo Json.
+        /// </summary>
+        /// <typeparam name="T">Objeto a ser deserializado</typeparam>
+        /// <param name="file">Arquivo json</param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string file)
+        {
+            try
+            {
+                var jsonFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), file);
+
+                return JsonConvert.DeserializeObject<T>(File.ReadAllText(jsonFilePath));
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+                throw;
+            }
+        }
+    }
+}
