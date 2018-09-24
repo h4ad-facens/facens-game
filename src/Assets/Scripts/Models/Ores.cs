ff28cd91d77696c20e39a6067e59e1f0d3f74ace
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-09-24 02:09:15 -0300

    Adicionado Models do Layer, LayerTiles e Ores

---
+++ src/Assets/Scripts/Models/Ores.cs
@@ -0,0 +1,40 @@
+﻿using Motherload.Core.Enums;
+
+namespace Motherload.Core.Models
+{
+    /// <summary>
+    /// Minério
+    /// </summary>
+    public class Ore
+    {
+        /// <summary>
+        /// Nome do minério
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Raridade do minério
+        /// </summary>
+        public Raritys Rarity { get; set; }
+
+        /// <summary>
+        /// Preço do minério
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Peso do minério
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// Pontos que o minério dá ao ser minerado
+        /// </summary>
+        public int Points { get; set; }
+
+        /// <summary>
+        /// Tipo do minério
+        /// </summary>
+        public OresTypes OreType { get; set; }
+    }
+}
